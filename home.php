d38d7fe81ddb0e2e36a41bfebf1af7c215d0e1e9
norsin <nt@bilandia.de>
2014-02-25 11:37:28 +0100

    #8727 - First Text Changes

--- home.php
+++ home.php
@@ -1,12 +1,12 @@
 <div class="contentWrapper eight columns offset-by-six">
 	<div class="content">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis diam id neque tincidunt adipiscing. Maecenas vel ornare sem. Nullam at ullamcorper ante, nec laoreet orci. In hac habitasse platea dictumst. Aenean elementum dolor et fermentum interdum. Pellentesque mollis ut velit in ultrices.</p>
+		<p>Stell Dir vor, Du lebst in einer Welt, in der Hexer und Hexen mit au&szlig;ergew&ouml;hnlichen magischen F&auml;higkeiten allgegenw&auml;rtig sind. Es gibt Wei&szlig;e und Schwarze Hexen, aber auch Halbcodes, denen man mit &auml;u&szlig;erstem Misstrauen begegnet, die zwischen Gut und B&ouml;se schwanken und sich entscheiden m&uuml;ssen.</p>
 		<p class="highlight">Mach mit und stell dich deiner dunkeln Seite!</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis diam id neque tincidunt adipiscing. Maecenas vel ornare sem. Nullam at ullamcorper ante, nec laoreet orci.</p>
+		<p>Unterziehe Dich dem Pers&ouml;nlichkeitstest, nimm an der Verlosung teil und gewinne eines von 20 Exemplaren von Sally Greens Roman &raquo;Half Bad &ndash; Das Dunkle in mir&laquo;!</p>
 		<form class="appNavigator" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 			<input type="hidden" name="backTo" value="home" />
-			<button class="mehrZumBuch button" name="currentPage" value="book"><span class="btnLabel">mehr zum Buch</span></button>
-			<button class="startGame button" name="currentPage" value="quiz"><span class="btnLabel">hier entlang</span></button>
+			<button class="mehrZumBuch button" name="currentPage" value="book"><span class="btnLabel">Mehr zum Buch</span></button>
+			<button class="startGame button" name="currentPage" value="quiz"><span class="btnLabel">Hier entlang</span></button>
 		</form>
 	</div>		
 </div>
