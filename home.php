bdfb379e658d33da9c0889199fb56c33e2897ee2
norsin <nt@bilandia.de>
2014-02-27 17:40:10 +0100

    #8727: Spelling

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <div class="contentWrapper eight columns offset-by-six">
 	<div class="content">
 		<p>Stell Dir vor, Du lebst in einer Welt, in der Hexer und Hexen mit au&szlig;ergew&ouml;hnlichen magischen F&auml;higkeiten allgegenw&auml;rtig sind. Es gibt Wei&szlig;e und Schwarze Hexen, aber auch Halbcodes, denen man mit &auml;u&szlig;erstem Misstrauen begegnet, die zwischen Gut und B&ouml;se schwanken und sich entscheiden m&uuml;ssen.</p>
-		<p class="highlight">Mach mit und stell dich deiner dunkeln Seite!</p>
+		<p class="highlight">Mach mit und stell dich deiner dunklen Seite!</p>
 		<p>Unterziehe Dich dem Pers&ouml;nlichkeitstest, nimm an der Verlosung teil und gewinne eines von 20 Exemplaren von Sally Greens Roman &raquo;Half Bad &ndash; Das Dunkle in mir&laquo;!</p>
 		<form class="appNavigator" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 			<input type="hidden" name="backTo" value="home" />
