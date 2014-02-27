93e522c79e52664583747155f93f03df447e7b3a
Giovanni Canzio <gc@grin.com>
2014-02-27 10:44:47 +0100

    #8727: Insert Leseprobe as file - split Leseprobe: Mobile Version goes to the PDF, Desktop Version to the cbt-site

--- book.php
+++ book.php
@@ -19,6 +19,7 @@
 		?>
 		<a href="#" target="_blank" class="button zumTrailer"><span class="btnLabel">Zum Trailer</span></a>
 		<a href="http://www.randomhouse.de/Buch/HALF-BAD-Das-Dunkle-in-mir-Band-1/Sally-Green/e448547.rhd?mid=4&exc=419500&showpdf=true#tabbox" target="_blank" class="button leseproben"><span class="btnLabel">Leseprobe</span></a>
+		<a href="pdf/leseprobe.pdf" target="_blank" class="button leseproben mobile"><span class="btnLabel">Leseprobe</span></a>
 		<a href="http://www.randomhouse.de/Buch/HALF-BAD-Das-Dunkle-in-mir-Band-1/Sally-Green/e448547.rhd" target="_blank" class="button kaufen"><span class="btnLabel">Kaufen</span></a>
 		<button class="button backToHome" name="currentPage" value="<?php echo $backTo?>"><span class="btnLabel">Zur&uuml;ck</span></button>
 	</form>
