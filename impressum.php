23eaebd1a8adf361734c1839da4d5338da00b3ec
Giovanni Canzio <gc@grin.com>
2014-06-04 16:40:26 +0200

    Issue #9962 : [Half Bad] Tab-Anpassung nach Kampagnenende - Teilnahmebedingungen bzw. Disclaimer löschen

--- impressum.php
+++ impressum.php
@@ -1,3 +1,3 @@
 <div class="container impressum">
-	<p>Veranstalter des Gewinnspiels ist allein der Verlag cbj in der Verlagsgruppe Random House. Das Gewinnspiel steht in keiner Verbindung zu Facebook und wird in keiner Weise von Facebook gesponsert, unterstützt oder organisiert.<br /><a href="http://www.randomhouse.de/randomhouse/agb_gewinnspiel.jsp" target="_blank" class="teilnahmeLink">Teilnahmebedingungen</a><br /><a href="http://www.randomhouse.de/impressum.jsp" target="_blank" class="impressumLink">Impressum</a></p>
+	<p><a href="http://www.randomhouse.de/impressum.jsp" target="_blank" class="impressumLink">Impressum</a></p>
 </div>
